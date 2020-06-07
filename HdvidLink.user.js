1781274c9a7eda46fcff4f6231af3d1e17da5dda
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2020-06-07 20:04:26 +0300

    Initial commit

---
+++ HdvidLink.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Hdvid Link
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @exclude     /^https?://hdvid.{1,3}/embed.*/
+// @include     /^https?://hdvid.*/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+if (location.href.indexOf("embed") !== -1) // NOTE: Don't run on image search
+{return;}
+var video_id=window.location.href.split('/')[3];
+window.location.href=video_id.replace(video_id,"embed-"+video_id)
+})();
+setTimeout(function(){alert(jwplayer("vplayer").getPlaylist()[0].file)},1000)
